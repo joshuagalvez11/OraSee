c8cb376e0667386e2b1a14cf7fdab0480f98bb0c
tbelyeu <tylerdbelyeu@gmail.com>
2022-09-21 11:59:39 -0700

    Agora video calling test page working
    Can directly connect to a channel using a hard-coded token. We will need to abstract this and set up a token server for full functionality.

--- orasee/lib/main.dart
+++ orasee/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import './src/pages/index.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -24,7 +26,8 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      //home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: IndexPage(),
     );
   }
 }
