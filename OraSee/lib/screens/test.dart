818fcb1d0b6192bd0be445624cc2951b4d4460b9
KQYang123 <yangkq0417@gmail.com>
2022-10-17 12:52:01 -0700

    'done1'

---
+++ OraSee/lib/screens/test.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class TestPage extends StatefulWidget {
+  const TestPage({super.key});
+
+  @override
+  State<TestPage> createState() => _TestPageState();
+}
+
+class _TestPageState extends State<TestPage> {
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onVerticalDragDown: (DragDownDetails details) {
+        Navigator.pop(context);
+      },
+      child: Container(
+        color: Colors.red,
+        
+      ),
+    );
+  }
+}
