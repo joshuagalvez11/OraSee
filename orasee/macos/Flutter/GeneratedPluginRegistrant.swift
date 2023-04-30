c16c07f52c83672514252c6dc259be5941907caa
Mohammed Chowdhury <65297339+MohammedSazzadChow@users.noreply.github.com>
2023-04-29 18:44:03 -0700

    Add files via upload

--- orasee/macos/Flutter/GeneratedPluginRegistrant.swift
+++ orasee/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,16 +5,10 @@
 import FlutterMacOS
 import Foundation
 
-import agora_rtc_engine
-import cloud_firestore
-import firebase_auth
-import firebase_core
-import package_info
+import path_provider_foundation
+import sqflite
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  AgoraRtcEnginePlugin.register(with: registry.registrar(forPlugin: "AgoraRtcEnginePlugin"))
-  FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
-  FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
-  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
-  FLTPackageInfoPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
