c0cef5f91e361df8005f39e87acc03e6f2ee2b56
tbelyeu <tylerdbelyeu@gmail.com>
2023-05-03 11:35:38 -0700

    ios config

--- orasee/ios/Flutter/flutter_export_environment.sh
+++ orasee/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/nabeelshehzad/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/nabeelshehzad/Downloads/Android/facebook"
+export "FLUTTER_ROOT=/Users/tylerbelyeu/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/tylerbelyeu/Documents/GitHub/OraSee/orasee"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
